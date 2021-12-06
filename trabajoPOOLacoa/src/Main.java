97f6ddaedb54e8a8066687e9360fe9a2e9aaeff2
Jose <jose.mercado@davinci.edu.ar>
2021-12-06 13:31:07 -0300

    Reestructuré el proyecto

--- trabajoPOOLacoa/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        CuentaBancaria cuenta = new CuentaBancaria("Juan", "94123456", 0);
-        cuenta.mostrarMenu();
-
-    }
-}
